a0b58132173156cd1b8cedcb9a0b7ba17d6cafac
Spong-e <alunjones@hotmail.com>
2022-03-20 17:36:30 +0000

    chore: savepoint

---
+++ src/steps/Step2.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+function Step2() {
+	return (
+		<div data-testid="step2">
+			<h1>Step 2</h1>
+			<p>This is Step 2.</p>
+		</div>
+	);
+}
+
+export default Step2;
---
+++ src/steps/Step3.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+
+function Step3() {
+    return 	<div data-testid="step3">
+    <h1>Step 3</h1>
+    <p>This is Step 3.</p>
+</div>
+}
+
+export default Step3;
